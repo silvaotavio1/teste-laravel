33ae54e5485d0ba6781827c767885956b1ecf9e8
silvaotavio1 <otaviosilva96@gmail.com>
2021-09-12 11:03:27 -0300

    no message

--- app/Indicacao.php
+++ app/Indicacao.php
@@ -2,10 +2,9 @@
 
 namespace App;
 
-// use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\Eloquent\Model;
 
-class Indicacao extends ModelNotFoundException
+class Indicacao extends Model
 {
-//
+
 }
