5462f24703dba4611fa8a0e5f125d193565bd457
silvaotavio1 <otaviosilva96@gmail.com>
2021-09-12 12:19:50 -0300

    no message

--- app/Http/Controllers/IndicacaoController.php
+++ app/Http/Controllers/IndicacaoController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use App\Indicacao as Indicacao;
 use App\IndicacoesStatus as IndicacoesStatus;
 use App\Http\Resources\Indicacao as IndicacaoResource;
+use App\Http\Resources\ValidaCPFCNPJ;
 use Illuminate\Http\Request;
-
 
 class IndicacaoController extends Controller
 {
@@ -26,9 +26,24 @@ class IndicacaoController extends Controller
   public function store(Request $request)
   {
     $indicacao = new Indicacao;
-    $indicacao->email_indicado = $request->input('email_indicado');
-    $indicacao->cpf_indicado = $request->input('cpf_indicado');
-    $indicacao->cpf_indica = $request->input('cpf_indica');
+
+    $email_indicado = $request->input('email_indicado');
+    $cpf_indicado = $request->input('cpf_indicado');
+    $cpf_indica = $request->input('cpf_indica');
+
+    $valida_cpf_indicado = new ValidaCPFCNPJ($cpf_indicado);
+    $valida_cpf_indica = new ValidaCPFCNPJ($cpf_indica);
+
+    if (!$valida_cpf_indica->valida()) {
+      return response()->json(['error' => 'CPF ou CNPJ Inválido [indica]'], 400);
+    } elseif (!$valida_cpf_indicado->valida()) {
+      return response()->json(['error' => 'CPF ou CNPJ Inválido [indicado]'], 400);
+    }
+
+    $indicacao->email_indicado = $email_indicado;
+    $indicacao->cpf_indicado = $cpf_indicado;
+    $indicacao->cpf_indica = $cpf_indica;
+
     if ($indicacao->save()) {
       return new IndicacaoResource($indicacao);
     }
@@ -51,17 +66,14 @@ class IndicacaoController extends Controller
 
     $status_id = $status_from < 3 ? ($status_from + 1) : $status_from;
 
-    if($status_from <> $status_id)
-    {
+    if ($status_from <> $status_id) {
       $IndicacoesStatus = new IndicacoesStatus;
       $IndicacoesStatus->status_id_from = $status_from;
       $IndicacoesStatus->status_id = $status_id;
       $IndicacoesStatus->user_alt = $request->user()['description'];
       $IndicacoesStatus->id = $request->id;
       $IndicacoesStatus->save();
-    }
-    else
-    {
+    } else {
       return response()->json(['error' => 'Indicação já esta no status FINALIZADA!'], 403);
     }
 
